3b67745e7f2344c57dd33562f7ad62659e8f5518
xingzhou <GTcTGqX3wMaGTZu>
2023-10-23 15:11:10 +0800

    精编

--- os kernel/principle-imp/【实验程序】linux-in-practice-master/06-storage-hierarchy/write.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -f testfile
-
-echo "$(date): start write (file creation)"
-dd if=/dev/zero of=testfile bs=1M count=1K
-echo "$(date): end write"
-
-rm -f testfile
--- os kernel/principle-imp/【实验程序】linux-in-practice-master/README.md
+++
@@ -1,3 +0,0 @@
-[《Linux是怎样工作的》](https://www.ituring.com.cn/book/2867)的实验用程序
-
-
